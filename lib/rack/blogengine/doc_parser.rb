102ad8390f66e6f813ea2c27e31619ca1041c4ef
Benny1992 <klotz.benjamin@yahoo.de>
2013-12-06 22:45:18 +0100

    added description to DocParser

--- lib/rack/blogengine/doc_parser.rb
+++ lib/rack/blogengine/doc_parser.rb
@@ -1,6 +1,14 @@
 module Rack
 	module Blogengine
 		class DocParser
+			# TODO Write Docparser
+			# return 
+			# {
+			#   path: "foo",
+			#   html:  HTML
+			# }
+			#
+			# HTML contains Content, Style, JS etc...
 		end
 	end
 end
