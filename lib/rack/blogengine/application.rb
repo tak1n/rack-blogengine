18e4b31e6d63047621c773ad73aa50e7c321d17e
Benny1992 <klotz.benjamin@yahoo.de>
2013-12-09 13:11:18 +0100

    call method -> Router call for response mapping, 404 if nothing found

--- lib/rack/blogengine/application.rb
+++ lib/rack/blogengine/application.rb
@@ -1,34 +1,15 @@
 module Rack
   module Blogengine
     class Application
-      def initialize(doc)
-        @doc_html = doc[:html]
-        @doc_path = doc[:path]
-      end
-                   
-	  def call(env)
-
-	    request = Rack::Request.new(env)
-	    headers = { 'Content-Type' => 'text/html' }
-
-	    #request.path to :path mapping
-	    #docdummy.each do |doc|
-	   		#if doc["path"] == request.path
-	   			#[200, headers, [doc["html"]] ]
-	   		#end
-	   	#end	
-
-	   	#if @found != true
-	   	#	[404, headers, ["Uh oh, path not found!"]]
-	   	#end
-	   	#
-	   	case request.path
-	    when @doc_path
-	      [200, headers, [@doc_html] ]
-	    else
-	      [404, headers, ["Uh oh, path not found!"]]
-	   	end
-  	   end                  
+		def call(env) 
+			route = ApplicationRouter.map_route(env) 
+			if route  
+				puts route
+				return route["response"] 
+			else 
+				return [404, {"Content-Type" => "text/html"}, ["Page not found"]] 
+			end 
+		end                
     end
   end
 end
