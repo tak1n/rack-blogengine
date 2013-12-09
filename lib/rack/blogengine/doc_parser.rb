614cf7543e914e6f3268e3ae132a48e5f086301d
Benny1992 <klotz.benjamin@yahoo.de>
2013-12-09 15:40:33 +0100

    DocParser reads in layout and substitutes layout placeholders with content from contentfiles

--- lib/rack/blogengine/doc_parser.rb
+++ lib/rack/blogengine/doc_parser.rb
@@ -9,6 +9,56 @@ module Rack
 			# }]
 			#
 			# HTML contains Content, Style, JS etc...
+			def self.parseInDocuments(target)
+				@target = target
+				documents = []
+
+				layout_file = ::File.open("#{@target}/layout/layout.html", "r")
+				@html = layout_file.read
+
+				Dir.foreach("#{target}/") do |item|
+					extension = item.split(".")[1]
+  					next if item == '.' or item == '..' or extension != "content"
+  					
+  					getFileContents(item)
+  					fillFileContents(@html)
+
+  					@document = {path: @path, html: @html}
+  					documents << @document
+				end
+
+				return documents
+			end
+
+			def self.getFileContents(file)
+				# do work on real items
+  				content_file = ::File.open("#{@target}/#{file}");
+  				content = content_file.read
+
+  				contentarray = content.split(",")
+
+  				contentarray.each do |contentblock|
+  					if contentblock.include? "[path]:"
+  						contentblock["[path]:"] = ""
+  						@path = "/#{contentblock}"
+  					end
+
+  					if contentblock.include? "[title]:"
+  						contentblock["[title]:"] = ""
+  						@title = contentblock
+  					end
+
+  					if contentblock.include? "[content]:"
+  						contentblock["[content]:"] = ""
+  						@content = contentblock.gsub(/\s+/, "")
+  					end
+  				end
+			end
+
+			def self.fillFileContents(layout)
+				layout.gsub! "{title}", @title
+				layout["{content}"] = @content
+			end
 		end
 	end
 end
