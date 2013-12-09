7b0227f088622525be927808dfb6c2738e72501c
Benny1992 <klotz.benjamin@yahoo.de>
2013-12-09 13:42:00 +0100

    removed debug output

--- lib/rack/blogengine/application.rb
+++ lib/rack/blogengine/application.rb
@@ -4,7 +4,6 @@ module Rack
 		def call(env) 
 			route = ApplicationRouter.map_route(env) 
 			if route  
-				puts route
 				return route["response"] 
 			else 
 				return [404, {"Content-Type" => "text/html"}, ["Page not found"]] 
