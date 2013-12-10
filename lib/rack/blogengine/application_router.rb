7322fad321ff4344417e31ea5e5a37c6c38de2bb
Benny1992 <klotz.benjamin@yahoo.de>
2013-12-10 10:26:57 +0100

    source styling

--- lib/rack/blogengine/application_router.rb
+++ lib/rack/blogengine/application_router.rb
@@ -1,27 +1,27 @@
 module Rack
   module Blogengine
     class ApplicationRouter 
-    	def self.map_route(env, target)
-    		status = 200
-    		header = {"Content-Type" => "text/html; charset=UTF-8"}
-    		path = env["PATH_INFO"]
+       def self.map_route(env, target)
+          status = 200
+    	  header = {"Content-Type" => "text/html; charset=UTF-8"}
+    	  path = env["PATH_INFO"]
 
-    		documents = DocParser.parseInDocuments(target)
+    	  documents = DocParser.parseInDocuments(target)
            
-            # Iterate through available docs, if nothing matched return nil
-    		documents.each do |doc|
-    			if doc[:path] == path
-    				route_response = {
-    					"route" => path,
-    					"response" => [status, header, [doc[:html]] ]
-    				}
+          # Iterate through available docs, if nothing matched return nil
+    	  documents.each do |doc|
+    	    if doc[:path] == path
+    		  route_response = {
+    		    "route" => path,
+    		    "response" => [status, header, [doc[:html]] ]
+    		  }
 
-    				return route_response
-    			end
+    		  return route_response
     		end
+    	  end
 
-            return nil
-    	end
+          return nil
+       end
     end
   end
 end
