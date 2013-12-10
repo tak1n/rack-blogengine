3c2320d51c7ea4c4772d8976b336d57206781c7d
Benny1992 <klotz.benjamin@yahoo.de>
2013-12-10 21:22:13 +0100

    refactored application.rb

--- lib/rack/blogengine/application.rb
+++ lib/rack/blogengine/application.rb
@@ -1,18 +1,18 @@
 module Rack
   module Blogengine
     class Application
-    	# Call Method for run this method as Rack Middleware.
-		# @param env Environment contains information such as path, headers etc...
-		# @return [Array] response Array
-		def self.call(env)
-			# Router for map docs to routes
-			route = ApplicationRouter.map_route(env, $targetfolder) 
-			if route  
-				return route["response"] 
-			else 
-				return [404, {"Content-Type" => "text/html; charset=UTF-8"}, ["Page not found"]] 
-			end 
-		end                
+      # Call Method for run this method as Rack Middleware.
+      # @param env Environment contains information such as path, headers etc...
+      # @return [Array] response Array
+      def self.call(env)
+        # Router for map docs to routes
+        route = ApplicationRouter.map_route(env, $targetfolder) 
+        if route  
+          return route["response"] 
+        else 
+          return [404, {"Content-Type" => "text/html; charset=UTF-8"}, ["Page not found"]] 
+        end 
+      end                
     end
   end
 end
