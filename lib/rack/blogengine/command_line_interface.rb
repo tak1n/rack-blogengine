1654fc4cf8a9d7542693682b1053ba3ac29101f2
Benny1992 <klotz.benjamin@yahoo.de>
2013-12-11 12:58:15 +0100

    moved cli logic into class CommandLineInterface

---
+++ lib/rack/blogengine/command_line_interface.rb
@@ -0,0 +1,26 @@
+module Rack
+  module Blogengine
+    class CommandLineInterface
+    	def run(target)
+    	  unless target.empty? 
+            $targetfolder = target
+            app = Rack::Builder.new do
+              use Rack::CommonLogger
+              use Rack::ShowExceptions
+
+              map "/assets" do
+                run Rack::Directory.new("#{$targetfolder}/assets")
+              end
+
+              use Rack::Lint
+              run Rack::Blogengine::Application
+            end
+
+            Rack::Server.start( :app => app )
+          else 
+            puts "Specify a targetfolder!"
+          end
+    	end
+    end
+  end
+end
