66d320114f8cf9f7c501062edddc6e5adb88355f
Benny1992 <klotz.benjamin@yahoo.de>
2013-12-15 09:28:08 +0100

    source styling

--- lib/rack/blogengine/command_line_interface.rb
+++ lib/rack/blogengine/command_line_interface.rb
@@ -17,23 +17,23 @@ module Rack
     	# @param [String] target
     	def run(target)
     	  unless target.empty? 
-            $targetfolder = target
-            app = Rack::Builder.new do
-              use Rack::CommonLogger
-              use Rack::ShowExceptions
+          $targetfolder = target
+          app = Rack::Builder.new do
+            use Rack::CommonLogger
+            use Rack::ShowExceptions
 
-              map "/assets" do
-                run Rack::Directory.new("#{$targetfolder}/assets")
-              end
-
-              use Rack::Lint
-              run Rack::Blogengine::Application
+            map "/assets" do
+              run Rack::Directory.new("#{$targetfolder}/assets")
             end
 
-            Rack::Server.start( :app => app, :Port => 3000 )
-          else 
-            puts "Specify a targetfolder!"
+            use Rack::Lint
+            run Rack::Blogengine::Application
           end
+
+          Rack::Server.start( :app => app, :Port => 3000 )
+        else 
+          puts "Specify a targetfolder!"
+        end
     	end
 
     	# TODO write generate method to generate blog skeleton (assets, layout etc)
