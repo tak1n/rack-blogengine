d7e4c52713aeb60625a9449066cccd8e1e9a0ce6
Benny1992 <klotz.benjamin@yahoo.de>
2013-12-24 21:21:56 +0100

    operator handling finished

--- lib/rack/blogengine/operator.rb
+++ lib/rack/blogengine/operator.rb
@@ -1,16 +1,14 @@
-libdir = "/var/www/rackblog/operator" # todo get operator dir via targetfolder
-
-Dir.foreach("#{libdir}/") do |item|
-	extension = item.split(".")[1]
-    next if item == '.' or item == '..' or extension != "rb"
-    puts item
-    require "#{libdir}/#{item}"
-end
-
 class Operator
-  extend UserOperator # load user operators
+  def initialize(target)
+    
+    Dir.foreach("#{target}operator/") do |item|
+	  extension = item.split(".")[1]
+      next if item == '.' or item == '..' or extension != "rb" 
+      require "#{target}operator/#{item}"
+    end
 
-  class << self
-  	# define default operators here
+    extend UserOperator # load user operators
   end
+  
+  # define default operators here
 end
