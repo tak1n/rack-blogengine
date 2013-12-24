8cbc1701d4edbd7d844c9577442ef4a80eedc935
Benny1992 <klotz.benjamin@yahoo.de>
2013-12-24 20:37:37 +0100

    Operator module, load UserOperators (todo operator path via targetfolder)

--- lib/rack/blogengine/operator.rb
+++ lib/rack/blogengine/operator.rb
@@ -1,2 +1,16 @@
+libdir = "/var/www/rackblog/operator" # todo get operator dir via targetfolder
+
+Dir.foreach("#{libdir}/") do |item|
+	extension = item.split(".")[1]
+    next if item == '.' or item == '..' or extension != "rb"
+    puts item
+    require "#{libdir}/#{item}"
+end
+
 class Operator
+  extend UserOperator # load user operators
+
+  class << self
+  	# define default operators here
+  end
 end
