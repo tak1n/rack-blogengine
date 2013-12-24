b50cfe6c5c04c5635f2a9f3f7baf9f95c1f284b5
Benny1992 <klotz.benjamin@yahoo.de>
2013-12-24 20:36:23 +0100

    add operator file to require

--- lib/rack/blogengine.rb
+++ lib/rack/blogengine.rb
@@ -3,6 +3,7 @@ require "rack/blogengine/doc_parser"
 require "rack/blogengine/document"
 require "rack/blogengine/application"
 require "rack/blogengine/application_router"
+require "rack/blogengine/operator"
 
 module Rack
   module Blogengine  
