a42661de9ef4d17f6cf0601c214b87b392cb2b7a
Benny1992 <klotz.benjamin@yahoo.de>
2013-12-24 20:37:08 +0100

    exec_operator calls method from Operator eigenmodule

--- lib/rack/blogengine/document.rb
+++ lib/rack/blogengine/document.rb
@@ -1,7 +1,6 @@
 module Rack
   module Blogengine
     class Document
-
       attr_accessor :path, :html, :title
 
       def to_hash
@@ -14,23 +13,7 @@ module Rack
 
       def exec_content_operator(documents)
         @html.scan(/\{\%(.*?)\%\}/).each do |operator|
-          case operator[0].strip
-          when "show_nav"
-            links = []
-
-            documents.each do |document|
-              puts document.path
-              puts document.title
-              link = "<li><a class='btn-primary' href='#{document.path}'>#{document.title}</a></li>"
-              links << link
-            end
-
-            nav = "<ul>"+links.join()+"</ul>"
-            
-            @html["{% show_nav %}"] = nav
-          else
-            #puts "Operator not known"
-          end
+          Operator.send(operator[0].strip.to_sym, documents, @html)
         end
       end
     end
