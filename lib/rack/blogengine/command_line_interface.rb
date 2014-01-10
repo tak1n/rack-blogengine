8e8405484233dd55c6cd1a75b152b7c574ed9555
Benny1992 <klotz.benjamin@yahoo.de>
2014-01-10 08:36:35 +0100

    generate method now working properly

--- lib/rack/blogengine/command_line_interface.rb
+++ lib/rack/blogengine/command_line_interface.rb
@@ -54,11 +54,57 @@ module Rack
 
       # Command to generate the folder skeleton
       # @param [String] folder
+      # TODO: refactor this - similar functions for each set up set => outsource to a method setup()
       def generate(folder)
-        puts "Generating folder skeleton"
-        system("mkdir #{folder} && mkdir #{folder}/assets && mkdir #{folder}/assets/layout && mkdir #{folder}/assets/js && mkdir #{folder}/assets/style && mkdir #{folder}/assets/images")
+        puts "\tGenerating folder skeleton\n"
+        system("mkdir #{folder}")
+        system("mkdir #{folder}/assets")
+        system("mkdir #{folder}/assets/layout")
+        system("mkdir #{folder}/assets/js")
+        system("mkdir #{folder}/assets/style")
+        system("mkdir #{folder}/assets/images")
+        system("mkdir #{folder}/operator")
+
+        puts "\n\tSetting up essential Files\n"
+
+        # SET UP operator.rb
+        puts "\tSet up #{folder}/operator/operator.rb\n"
+        system("touch #{folder}/operator/operator.rb")
+        content = IO.read("#{::File.dirname(__FILE__)}/files/operator.rb")
+        ::File.open("#{folder}/operator/operator.rb", 'w') { |file| file.write(content) }
+
+        # SET UP config.yml
+        puts "\tSet up config.yml\n"
+        system("touch #{folder}/config.yml")
+        content = IO.read("#{::File.dirname(__FILE__)}/files/config.yml")
+        ::File.open("#{folder}/config.yml", 'w') { |file| file.write(content) }
+
+        # SET UP index.content
+        puts "\tSet up #{folder}/index.content\n"
+        system("touch #{folder}/index.content")
+        content = IO.read("#{::File.dirname(__FILE__)}/files/index.content")
+        ::File.open("#{folder}/index.content", 'w') { |file| file.write(content) }
+
+        # SET UP layout.html
+        puts "\tSet up #{folder}/assets/layout/layout.html\n"
+        system("touch #{folder}/assets/layout/layout.html")
+        content = IO.read("#{::File.dirname(__FILE__)}/files/layout.html")
+        ::File.open("#{folder}/assets/layout/layout.html", 'w') { |file| file.write(content) }
+
+        # SET UP style.css
+        puts "\tSet up #{folder}/assets/style/style.css\n"
+        system("touch #{folder}/assets/style/style.css")
+
+        # SET UP style.css
+        puts "\tSet up #{folder}/assets/js/script.js\n"
+        system("touch #{folder}/assets/js/script.js")
+
+        puts "\n\tSetup finished! Have Fun\n"
+        puts "\tTo test it type rack-blogengine run #{folder}"
       end
 
+      # Display Version
+      # return [String] VERSION
       def version?
         puts Rack::Blogengine::VERSION
       end
