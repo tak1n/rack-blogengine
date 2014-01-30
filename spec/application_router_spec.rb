c11d69c95d0072518dcf1706e271d7555e67f1ef
Benny1992 <klotz.benjamin@yahoo.de>
2014-01-30 11:35:20 +0100

    update

--- spec/application_router_spec.rb
+++ spec/application_router_spec.rb
@@ -1,5 +1,11 @@
 require 'spec_helper'
 
-describe Rack::Blogengine::ApplicationRouter do
-  pending "write it"
+describe Array do
+  it "can be created with no arguments" do
+    Array.new.must_be_instance_of(Array)
+  end
+
+  it "can be created with a specific size" do
+    Array.new(10).size.must_equal(10)
+  end
 end
