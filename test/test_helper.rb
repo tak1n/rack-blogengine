94b2b6e788f655b8edb4f2611b8c00b235740373
Benny1992 <klotz.benjamin@yahoo.de>
2014-01-30 11:07:15 +0100

    changed to require

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -2,4 +2,5 @@ require 'minitest/autorun'
 require 'minitest/pride' # for colored output
 require 'minitest/unit'
 
-require_relative '../lib/rack/blogengine.rb'
+#require_relative '../lib/rack/blogengine.rb'
+require 'rack-blogengine'
