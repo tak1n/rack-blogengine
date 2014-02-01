c417f06441c04b92f392210d9ae0ab080bb04e06
Benny1992 <klotz.benjamin@yahoo.de>
2014-02-01 12:02:02 +0100

    add some unit tests

--- test/application_router_test.rb
+++ test/application_router_test.rb
@@ -1,17 +1,38 @@
 require 'test_helper.rb'
 
 class ApplicationRouterTest < Test::Unit::TestCase
+  include Rack::Test::Methods
+
   def setup
-  	puts "Path is: #{testpath}"
-  	@document = Rack::Blogengine::DocParser.parseInDocuments(testpath)
+  	documents = Rack::Blogengine::DocParser.parseInDocuments(testpath)
+    env_fail = { "PATH_INFO" => "/fail" }
+    env_success = { "PATH_INFO" => "/" }
+
+    @route = Rack::Blogengine::ApplicationRouter.map_route(env_success, documents)
   end
 
-  def test_map_route
-  	puts @document
+  def test_map_route_general
+    assert_equal( Hash, @route.class, "Route should be a hash")
+
+    # Check Hash keys
+    assert_true(@route.has_key?("path"), "Route should contain a path")
+    assert_true(@route.has_key?("response"), "Route should contain a response")
+
+    # Check path
+    assert_equal( String, @route["path"].class, "Path should be a string")
+
+    # Check response
+    assert_equal( Array, @route["response"].class, "Response should be an Array")
+
+    
+    assert_equal( Fixnum, @route["response"][0].class, "Status should be a Fixnum")
+  end
+
+  def test_map_route_on_success
+    assert_equal( 200, @route["response"][0], "Status should be 200")
   end
 
   def teardown
-  	puts Rack::Blogengine.root
-    system("rm -rf #{Rack::Blogengine.root}/testfolder")
+    #system("rm -rf #{testpath}")
   end
 end
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -9,14 +9,31 @@ require 'test/unit/notify'
 
 
 require 'rack/blogengine'
+require 'rack/test'
 
 # alternative to require_relative
 # rake install in gem dir and then
 # require 'mygem'
 
-def testpath
- return "#{Rack::Blogengine.root}/testfolder"
+# class Hash
+  # method for usage with assert_boolean 
+  # -> for failing it needs nil instead of false (which #has_key? returns in failing case)
+  # NOT Needed use assert_true instead...
+  # def hash_key?(key)
+  #   if self.has_key?(key)
+  #	    return true
+  #	  else 
+  #	    return nil
+  #	  end
+  # end
+# end
+
+class Object
+  def testpath
+    return "#{Rack::Blogengine.root}/testfolder"
+  end
 end
+
 
 def capture_stdout(&block)
   original_stdout = $stdout
