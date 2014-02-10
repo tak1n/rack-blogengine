ceab714a61f435f092763767a94093fecd4aac3b
Benny1992 <klotz.benjamin@yahoo.de>
2014-02-10 06:56:11 +0100

    added more rspec tests

--- spec/document_spec.rb
+++ spec/document_spec.rb
@@ -10,12 +10,12 @@ describe Rack::Blogengine::Document do
     @document.html = '<html><h1>Test</h1></html>'
   end
 
-  describe "#new" do
-    it "should be an instance of Document" do
+  describe '#new' do
+    it 'should be an instance of Document' do
       @document.class.should eql Rack::Blogengine::Document
     end
 
-    it "should have content when parsed in" do
+    it 'should have content when parsed in' do
       @document.title.should eql 'testtitle'
       @document.path.should eql '/test'
       @document.date.should eql '20-20-2014'
@@ -23,12 +23,12 @@ describe Rack::Blogengine::Document do
     end
   end
 
-  describe "#to_hash" do
-  	it "should return the right hash" do
-  	  hashed = @document.to_hash
+  describe '#to_hash' do
+    it 'should return the right hash' do
+      hashed = @document.to_hash
 
-  	  hashed.key?(:path).should be_true
-  	  hashed.key?(:html).should be_true
-  	end
+      hashed.key?(:path).should be_true
+      hashed.key?(:html).should be_true
+    end
   end
 end
