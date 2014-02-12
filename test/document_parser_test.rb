691482d0694a99710f2e2a52257439e2ce3eae90
Benny1992 <klotz.benjamin@yahoo.de>
2014-02-12 21:10:42 +0100

    added fill_file_contents test

--- test/document_parser_test.rb
+++ test/document_parser_test.rb
@@ -24,6 +24,20 @@ class DocumentParserTest < MiniTest::Unit::TestCase
   # Single Test method for each class method in document_parser.rb ?
   # parse_in_documents calls other class methods so when something is wrong with other class method
   # test_parse_in_documents will fail => so for now only 1 test here
+  
+  def test_fill_file_contents
+    layout_file = ::File.open("#{testpath}/assets/layout/layout.html", 'r')
+    layout = layout_file.read
+    title = 'testtitle'
+    content = 'testcontent'
+    date = Date.new()
+
+    html = Rack::Blogengine::DocumentParser.fill_file_contents(layout, title, content, date)
+
+    assert(html.include?(title), 'Parsed and filled in HTML should contain Title')
+    assert(html.include?(content), 'Parsed and filled in HTML should contain Content')
+    assert(html.include?(date.strftime('%d.%m.%Y')), 'Parsed and filled in HTML should contain Date')
+  end
 
   def teardown
     system("rm -rf #{testpath}")
