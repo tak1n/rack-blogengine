2a69f3ac26b08fa54810687affa3071055513b28
Benny1992 <klotz.benjamin@yahoo.de>
2014-02-20 11:02:27 +0100

    add Rack::Blogengine.server_thread to env

--- features/support/env.rb
+++ features/support/env.rb
@@ -19,6 +19,20 @@ end if ENV["COVERAGE"]
 require 'capybara/cucumber'
 require 'rack/blogengine'
 
+
+module Rack
+  module Blogengine
+    class << self
+      attr_accessor :server_thread
+    end
+  end
+end 
+
 Rack::Blogengine.documents = [{ html: '<!DOCTYPE html><body><h2>index</h2></body></html>',
                                 path: '/' }]
+
+Rack::Blogengine.server_thread = Thread.new do
+  @cli.send(:run, "#{Rack::Blogengine.root}/testfolder")
+end
+
 Capybara.app = Rack::Blogengine::Application
