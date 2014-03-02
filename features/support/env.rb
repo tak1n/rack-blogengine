34089ba36c83584112b70abd3399be73f5152710
Benny1992 <klotz.benjamin@yahoo.de>
2014-03-02 22:02:24 +0100

    Application call is now a instance method not a class method anymore - fix in cucumber support/env.rb

--- features/support/env.rb
+++ features/support/env.rb
@@ -22,4 +22,4 @@ require 'rack/blogengine'
 Rack::Blogengine.documents = [{ html: '<!DOCTYPE html><body><h2>index</h2></body></html>',
                                 path: '/' }]
 
-Capybara.app = Rack::Blogengine::Application
+Capybara.app = Rack::Blogengine::Application.new
