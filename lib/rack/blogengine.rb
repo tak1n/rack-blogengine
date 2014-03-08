2764459472e3e3e66e7590d256f901b5c2525dc4
Benny1992 <klotz.benjamin@yahoo.de>
2014-03-08 11:45:05 +0100

    root method bugfix...

--- lib/rack/blogengine.rb
+++ lib/rack/blogengine.rb
@@ -24,7 +24,7 @@ module Rack
       # Method to return Gem Root Dir
       # @return [String] Gem Root Folder
       def root
-        ::File.dirname(::File.dirname(::File.expand_path('../..', __FILE__)))
+        ::File.dirname(::File.dirname(::File.expand_path('..', __FILE__)))
       end
     end
   end
